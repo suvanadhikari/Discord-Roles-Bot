963d3ef8298d21a2e2cf02314cfd9dfe43389baf
suvanadhikari <53096723+suvanadhikari@users.noreply.github.com>
2021-04-28 00:53:17 -0400

    Consistent indentation

--- keep_alive.py
+++ keep_alive.py
@@ -5,11 +5,11 @@ app = Flask('')
 
 @app.route('/')
 def home():
-  return "Bot live."
+     return "Bot live."
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+     app.run(host='0.0.0.0',port=8080)
 
 def keep_alive():
-  t = Thread(target=run)
-  t.start()
+     t = Thread(target=run)
+     t.start()
